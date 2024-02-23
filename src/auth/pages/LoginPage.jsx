9608e49838c3d4f1d5a196735efc43643e661762
jenri019 <j.enrique209436@hotmail.com>
2024-02-23 12:11:23 -0600

    Starting LoginPage without layouts

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,3 +1,41 @@
+import { Grid, TextField, Typography } from '@mui/material'
+import React from 'react'
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
-};
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction='column'
+      alignItems='center'
+      justifyContent='center'
+      sx={{minHeight: '100vh', backgroundColor: 'primary.main', padding: 4}}
+    >
+      <Grid item
+        clasName='box-shadow'
+        xs={3}
+        sx={{backgroundColor: '#ffffff', padding: 3, borderRadius: 2}}
+      >
+        <Typography variant='h5' sx={{mb: 1}}>Login</Typography>
+        <form>
+          <Grid container>
+            <Grid item xs={12} sx={{mt: 2}}>
+              <TextField
+                label='correo'
+                type='email'
+                placeholder='correo@gmail.com'
+                fullWidth />
+            </Grid>
+            <Grid item xs={12} sx={{mt: 2}}>
+              <TextField
+                label='contraseña'
+                type='password'
+                placeholder='contraseña'
+                fullWidth />
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
+  )
+}
