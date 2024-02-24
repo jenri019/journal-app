140d876a490d537c6bddc9192312eae9fd1b22b6
jenri019 <j.enrique209436@hotmail.com>
2024-02-24 11:59:20 -0600

    Creating JournalLayout

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,24 @@
+import { Box, Toolbar } from '@mui/material'
+import React from 'react'
+import { Navbar, Sidebar } from '../components';
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({ children }) => {
+    return (
+        <Box sx={{ display: 'flex' }}>
+
+            <Navbar drawerWidth={drawerWidth}/>
+
+            <Sidebar drawerWidth={drawerWidth}/>
+
+            <Box
+                component='main'
+                sx={{ flexGrow: 1, p: 3 }}
+            >
+                <Toolbar />
+                {children}
+            </Box>
+        </Box>
+    )
+}
