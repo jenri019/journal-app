c1005e2c2d05704b6d8dbace3fbf35b81abb5668
jenri019 <j.enrique209436@hotmail.com>
2024-02-24 12:49:49 -0600

    Adding float button, modifying theme

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
+import { IconButton } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NoteView />
+      <NothingSelectedView />
+      <IconButton
+        size='large'
+        sx={{
+          color: '#ffffff',
+          backgroundColor: 'primary.button',
+          ':hover': { backgroundColor: 'primary.button', opacity: 0.8 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   )
 }
--- src/theme/purpleTheme.js
+++ src/theme/purpleTheme.js
@@ -4,10 +4,11 @@ import { red } from "@mui/material/colors";
 export const purpleTheme = createTheme({
     palette: {
         primary: {
-            main: '#262254'
+            main: '#543884',
+            button: '#262254'
         },
         secondary: {
-            main: '#543884'
+            main: '#8551e0'
         },
         error: {
             main: red.A400
