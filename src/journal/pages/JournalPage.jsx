d25c9e540d263b82fe7a085aa73e4c88cf27111e
jenri019 <j.enrique209436@hotmail.com>
2024-02-24 11:59:33 -0600

    Adding content to JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
+import { NoteView, NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <NoteView />
+    </JournalLayout>
   )
 }
